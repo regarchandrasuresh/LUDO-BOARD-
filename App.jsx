ef2a1628101e27a307cafd0eb4e406e9cc05692d
Suresh Regar <144477857+regarchandrasuresh@users.noreply.github.com>
2025-08-20 19:51:27 +0530

    App.jsx

---
+++ App.jsx
@@ -0,0 +1,15 @@
+
+import React from "react";
+import "./style.css";
+import LudoBoard from "./LudoBoard";
+
+export default function App() {
+  return (
+    <div>
+    <LudoBoard/>
+    </div>
+  );
+}
+
+
+
